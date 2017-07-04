da7888b180cb407db0ff07cf7e7174d77eaac6d6
Vitaliy Ostapenko <soul33d@gmail.com>
2017-07-05 02:32:30 +0300

    fully realized singleton

---
+++ src/com/homelearning/singleton/ActionJournal.java
@@ -0,0 +1,35 @@
+package com.homelearning.singleton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActionJournal {
+
+    private List<String> actionList;
+
+    private ActionJournal() {
+        actionList = new ArrayList<>();
+    }
+
+    public void addAction(String action){
+        actionList.add(action);
+    }
+
+    public void undoAction(){
+        actionList.remove(actionList.size() - 1);
+    }
+
+    @Override
+    public String toString() {
+        return "ActionJournal{" + actionList +
+                '}';
+    }
+
+    public static ActionJournal getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    private static class SingletonHolder {
+        private static final ActionJournal instance = new ActionJournal();
+    }
+}
