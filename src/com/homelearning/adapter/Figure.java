9fc72c3c87ed4ee5c679cd695d6362e3361c1819
Vitaliy Ostapenko <soul33d@gmail.com>
2017-07-07 00:15:16 +0300

    fully realized adapter

---
+++ src/com/homelearning/adapter/Figure.java
@@ -0,0 +1,26 @@
+package com.homelearning.adapter;
+
+public class Figure {
+    double x,y;
+
+    public Figure() {
+    }
+
+    public Figure(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(double xInches, double yInches){
+        x += xInches;
+        y += yInches;
+    }
+
+    @Override
+    public String toString() {
+        return "Figure{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
